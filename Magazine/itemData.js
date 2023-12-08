459d7ef1365a3ee4127e124ffec432684ec8e1f3
Roman <sup.makulin@mail.ru>
2023-12-09 00:07:23 +0300

    create card items on click

--- Magazine/itemData.js
+++ Magazine/itemData.js
@@ -2,31 +2,31 @@
 const itemsData = `[{"img": "./img/item1.jpg",
 "title": "ELLERY X MO CAPSULE",
 "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-"price": "$52.00"
+"price": "$52.00", "color": "black", "size": "XL"
 }, {
     "img": "./img/item2.jpg",
     "title": "ELLERY X M'O CAPSULE",
     "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price": "$52.00"
+    "price": "$52.00", "color": "red", "size": "L"
 }, {
     "img": "./img/item3.jpg",
     "title": "ELLERY X M'O CAPSULE",
     "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price": "$52.00"
+    "price": "$52.00", "color": "white", "size": "M"
 }, {
     "img": "./img/item4.jpg",
     "title": "ELLERY X M'O CAPSULE",
     "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price": "$52.00"
+    "price": "$52.00", "color": "black", "size": "S"
 }, {
     "img": "./img/item5.jpg",
     "title": "ELLERY X M'O CAPSULE",
     "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price": "$52.00"
+    "price": "$52.00", "color": "white", "size": "XS"
 }, {
     "img": "./img/item6.jpg",
     "title": "ELLERY X M'O CAPSULE",
     "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "price": "$52.00"
+    "price": "$52.00", "color": "black", "size": "XXL"
 }
 ]`;
--- Magazine/script.js
+++ Magazine/script.js
@@ -18,6 +18,19 @@ titleEl.appendChild(containerSubTitleEl);
 
 containerEl.appendChild(titleEl);
 
+// 
+
+const bucketContainer = document.querySelector(".user-bucket");
+const bucketTitle = document.createElement("h2");
+
+bucketTitle.innerHTML = "Card items";
+bucketTitle.classList.add('items_title_name');
+
+bucketContainer.classList.add("basket_left");
+bucketContainer.appendChild(bucketTitle);
+
+// 
+
 itemsArr.forEach(element => {
     const imgEl = document.createElement("img");
     const titleEl = document.createElement("h4");
@@ -30,7 +43,7 @@ itemsArr.forEach(element => {
     titleEl.innerHTML = element.title;
     textEl.innerHTML = element.text;
     priceEl.innerHTML = element.price;
-    addCartEl.innerHTML = svgLink;
+    addCartEl.innerHTML = addCardSvg;
 
     elementDiv.classList.add('items_cart');
     imgEl.classList.add('items_cart_img');
@@ -46,6 +59,74 @@ itemsArr.forEach(element => {
     elementDiv.appendChild(addCartEl);
 
     containerEl.appendChild(elementDiv);
+
+    addCartEl.addEventListener("click", () => {
+
+        const divCart = document.createElement("div");
+
+        const imgCart = document.createElement("img");
+
+        const nullDiv = document.createElement("div");
+        const titleCart = document.createElement("h4");
+        const priceCart = document.createElement("p");
+        const colorCart = document.createElement("p");
+        const sizeCart = document.createElement("p");
+
+        const countCart = document.createElement("div");
+        const countTextCart = document.createElement("p");
+        const countInputCart = document.createElement("input");
+
+        const closeCart = document.createElement("a");
+
+        imgCart.src = element.img;
+        titleCart.innerHTML = element.title;
+        priceCart.innerText = element.price;
+        colorCart.innerText = element.color;
+        sizeCart.innerText = element.size;
+        countTextCart.innerText = "Quantity: ";
+        closeCart.innerHTML = closeCardSvg;
+
+        // classes
+        divCart.classList.add("basket_item");
+        imgCart.classList.add("basket_item_img");
+        titleCart.classList.add("basket_item_title");
+        priceCart.classList.add("basket_item_text");
+        colorCart.classList.add("basket_item_text");
+        sizeCart.classList.add("basket_item_text");
+
+        countCart.classList.add("basket_item_count");
+        countTextCart.classList.add("basket_item_text");
+
+        countInputCart.classList.add("basket_item_text_count");
+        countInputCart.name = "count";
+        countInputCart.type = "number";
+        countInputCart.min = 2;
+
+        closeCart.classList.add("basket_item_close");
+
+        // append DIV
+        countCart.appendChild(countTextCart);
+        countCart.appendChild(countInputCart);
+
+        // append DIV
+        divCart.appendChild(imgCart);
+
+        nullDiv.appendChild(titleCart);
+        nullDiv.appendChild(priceCart);
+        nullDiv.appendChild(colorCart);
+        nullDiv.appendChild(sizeCart);
+        nullDiv.appendChild(countCart);
+
+        divCart.appendChild(nullDiv);
+        divCart.appendChild(closeCart);
+
+        bucketContainer.appendChild(divCart);
+
+        closeCart.addEventListener("click", () => {
+            divCart.remove();
+        });
+
+    });
 });
 
 const browseEl = document.createElement("a");
